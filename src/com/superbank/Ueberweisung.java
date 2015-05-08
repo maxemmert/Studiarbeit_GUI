68df40eba9b6904927f06dcc512b0ba7145269f1
Daniel Auler <daniel-auler@t-online.de>
2015-05-08 13:56:33 +0200

    Update GUI Überweisung

--- src/com/superbank/Ueberweisung.java
+++ src/com/superbank/Ueberweisung.java
@@ -1,55 +1,71 @@
 package com.superbank;
 
-import com.example.superbank.R;
-
 import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.RadioButton;
 
-public class Ueberweisung extends Fragment{
-   
-	     /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+import com.example.superbank.R;
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static Ueberweisung newInstance(int sectionNumber) {
-        	Ueberweisung fragment = new Ueberweisung();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
+public class Ueberweisung extends Fragment {
 
-        public Ueberweisung() {
-        }
+	/**
+	 * The fragment argument representing the section number for this fragment.
+	 */
+	private static final String ARG_SECTION_NUMBER = "section_number";
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-        	
-        	View rootView = inflater.inflate(R.layout.fragment_ueberweisung, container, false);
-         return rootView;
-        }
+	/**
+	 * Returns a new instance of this fragment for the given section number.
+	 */
+	public static Ueberweisung newInstance(int sectionNumber) {
+		Ueberweisung fragment = new Ueberweisung();
+		Bundle args = new Bundle();
+		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+		fragment.setArguments(args);
+		return fragment;
+	}
 
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
+	public Ueberweisung() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_ueberweisung,
+				container, false);
+
+		if (MainActivity.bankname1 != "") {
+			RadioButton radioKto1 = (RadioButton) rootView
+					.findViewById(R.id.radiokonto1);
+			radioKto1.setText(MainActivity.bankname1);
+			radioKto1.setVisibility(View.VISIBLE);
+		}
+
+		if (MainActivity.bankname2 != "") {
+			RadioButton radioKto2 = (RadioButton) rootView
+					.findViewById(R.id.radiokonto2);
+			radioKto2.setText(MainActivity.bankname2);
+			radioKto2.setVisibility(View.VISIBLE);
+		}
+
+		if (MainActivity.bankname3 != "") {
+			RadioButton radioKto3 = (RadioButton) rootView
+					.findViewById(R.id.radiokonto3);
+			radioKto3.setText(MainActivity.bankname3);
+			radioKto3.setVisibility(View.VISIBLE);
+		}
+
+		return rootView;
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		((MainActivity) activity).onSectionAttached(getArguments().getInt(
+				ARG_SECTION_NUMBER));
+	}
 }
